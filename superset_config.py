412643ada9a30cc7ee4a20f365bf9f20b2f4b2cf
Kirill Pavlov <kirill@terminal1.co>
2018-05-12 23:50:33 +0800

    ADD: superset config with PostgreSQL and redis

---
+++ superset_config.py
@@ -0,0 +1,11 @@
+CACHE_CONFIG = {
+    'CACHE_TYPE': 'redis',
+    'CACHE_DEFAULT_TIMEOUT': 300,
+    'CACHE_KEY_PREFIX': 'superset_',
+    'CACHE_REDIS_HOST': 'redis',
+    'CACHE_REDIS_PORT': 6379,
+    'CACHE_REDIS_DB': 1,
+    'CACHE_REDIS_URL': 'redis://redis:6379/1'
+}
+SQLALCHEMY_DATABASE_URI = \
+    'postgresql+psycopg2://postgres:postgres@postgres:5432/postgres'
